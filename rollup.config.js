beb0893129d668fb29608c49de3442b53c6f93e5
evgeniybabiychuk01 <94927400+evgeniybabiychuk01@users.noreply.github.com>
2024-05-02 10:49:42 +0700

    update

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,13 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from 'rollup-plugin-postcss';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss'
 
-const packageJson = require("./package.json");
+
+import packageJson from './package.json' assert { type: "json" };
+import tailwindConfig from './tailwind.config.js';
+
 
 export default [
     {
@@ -27,21 +32,15 @@ export default [
         commonjs(),
         typescript({ tsconfig: "./tsconfig.json" }),
         postcss({
-            config: {
-                path: './postcss.config.js',
-            },
-            extensions: ['.css'],
-            minimize: true,
-            inject: {
-                insertAt: 'top',
-            },
-        })
+            extensions: ['.css', '.module.css'],
+            plugins: [autoprefixer(), tailwindcss(tailwindConfig)],
+        }),
     ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts.default()],
+        plugins: [dts()],
         external: [/\.css$/]
     },
 ]
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
+import "./index.module.css";
 export * from "./components";
