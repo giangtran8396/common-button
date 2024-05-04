d81291ff22e5108299670edfcc9778284817667f
giangtlq <gtran6969@gmail.com>
2024-05-04 13:15:38 +0700

    update

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,8 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from 'rollup-plugin-postcss';
 import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss'
-
+import tailwindcss from 'tailwindcss';
+import generatePackageJSON from 'rollup-plugin-generate-package-json';
 
 import packageJson from './package.json' assert { type: "json" };
 import tailwindConfig from './tailwind.config.js';
@@ -30,11 +30,32 @@ export default [
             extensions: ['.css', '.module.css'],
             plugins: [autoprefixer(), tailwindcss(tailwindConfig)],
         }),
+        generatePackageJSON({
+            //create new json file
+            outputFolder: "shared-component",
+            baseContents: (pkg) => ({
+              name: pkg.name,
+              version: packageJson.version,
+              author: packageJson.author,
+              license: packageJson.license,
+              main: "dist/index.js",
+              peerDependencies: {
+                react: packageJson.devDependencies.react,
+              },
+              type: packageJson.type,
+              //should start from dist
+              module: "dist/index.js",
+              files: packageJson.files,
+              //should start from dist
+              types: "dist/index.d.ts",
+              publishConfig: packageJson.publishConfig
+            }),
+        }),
     ],
     },
     {
-        input: "dist/types/index.d.ts",
-        output: [{ file: "dist/index.d.ts", format: "esm" }],
+        input: "shared-component/dist/types/index.d.ts",
+        output: [{ file: "shared-component/dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
     },
 ]
